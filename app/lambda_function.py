bdc0f4cb160a9794d879c114fcbb46ed22ca6a78
Todor Lubenov <todor.lubenov@cytora.com>
2021-07-06 21:13:24 +0300

    change request handler

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -5,16 +5,16 @@ from mangum import Mangum
 
 from api import app
 
+lambda_handler = Mangum(app=app)
 
+'''
 def lambda_handler(event, context):
     debug = os.getenv('DEBUG', '')
     if debug.lower() == 'true':
         evt = json.dumps(event)
         print(f'{evt}')
-    if event.get("some-key"):
-        # Do something or return, etc.
-        return
 
     asgi_handler = Mangum(app)
     response = asgi_handler(event, context) # Call the instance with the event arguments
     return response
+'''
